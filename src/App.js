ad50415dc1334bcdcc7938b38e0521d8c9b9a169
kariamyasser <38008930+kariamyasser@users.noreply.github.com>
2020-07-05 21:20:27 +0200

    Added admins table component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AdminsTable from './components/admins.table'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Box from '@material-ui/core/Box';
 
@@ -20,6 +21,7 @@ function App() {
             <Route exact path='/' component={SignIn} />
             <Route exact path="/SignIn" component={SignIn} />
             <Route exact path='/Vis' component={Topology} />
+            <Route exact path='/admins' component={AdminsTable} />
         <Container>
           <Box mt={8}>
             <Copyright />
